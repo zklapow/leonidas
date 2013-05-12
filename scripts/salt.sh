7794fa6e289829d94a309fbd6f3da05916a6f554
Ze'ev Klapow <zklapow@gmail.com>
2013-05-12 15:25:44 -0400

    Better minion bootsrap script.

--- scripts/salt.sh
+++ scripts/salt.sh
@@ -4,9 +4,24 @@
 add-apt-repository ppa:saltstack/salt -y
 apt-get update -y
 apt-get install salt-minion -y
-apt-get install salt-master -y
 apt-get upgrade -y
 
 # Set salt master location and start minion
 sed -i 's/#master: salt/master: salt.coherentclothes.com/' /etc/salt/minion
-salt-minion -d
+
+# Explicitly set the id
+sed -i 's/#id:/id: %s/' /etc/salt/minion
+
+# Set the keys
+read -d '' PEM <<_EOF_
+%s
+_EOF_
+
+read -d '' PUB <<_EOF_
+%s
+_EOF_
+
+echo "${PEM}" > /etc/salt/pki/minion/minion.pem
+echo "${PUB}" > /etc/salt/pki/minion/minion.pub
+
+service salt-minion restart
